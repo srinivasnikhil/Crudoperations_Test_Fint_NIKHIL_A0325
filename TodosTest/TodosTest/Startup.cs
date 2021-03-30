0d52be343939322461616a256ce1a851a7c133a8
Nikhil Mallempati <srinivasnikhil38@gmail.com>
2021-03-30 11:57:37 +0530

    added cors

--- TodosTest/TodosTest/Startup.cs
+++ TodosTest/TodosTest/Startup.cs
@@ -41,6 +41,12 @@ namespace TodosTest
 
             app.UseRouting();
 
+            app.UseCors(x => x
+                            .AllowAnyMethod()
+                            .AllowAnyHeader()
+                            .SetIsOriginAllowed(origin => true)
+                            .AllowCredentials());
+
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
